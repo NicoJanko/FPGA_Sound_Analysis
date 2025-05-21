478e8c446dcc5713bacfdd69c14b095d56ca07b8
NicoJanko <nicolas.jankovsky@gmail.com>
2025-05-21 09:27:07 +0200

    last version

--- adc_to_uart.v
+++ adc_to_uart.v
@@ -1,9 +1,9 @@
 module adc_to_uart (
     input wire clk,               // 12 MHz clock
     input wire ready,             // XADC drdy_out: new sample ready
-    input wire [15:0] i_xdac,     // 12-bit ADC output (upper bits valid)
+    input wire [15:0] i_xdac,     // Raw XADC output
     output wire uart_tx,          // UART TX line to PC
-    output wire busy              // High when transmitting
+    output wire busy              // UART busy status
 );
 
     parameter CLK_FREQ = 12000000;
@@ -11,16 +11,16 @@ module adc_to_uart (
     localparam SAMPLE_DELAY = CLK_FREQ / SAMPLE_FREQ;
 
     reg [15:0] sample_delay = 0;
-    reg [15:0] adc_sample = 0;
+    reg [11:0] adc_raw = 0;               // Correct 12-bit value
+    reg [15:0] adc_sample = 0;            // Padded for UART
     reg [7:0] tx_byte = 0;
     reg send = 0;
 
-    reg [1:0] state = 0;  // FSM state: 0 = start, 1 = MSB, 2 = LSB
+    reg [1:0] state = 0;
 
     wire uart_busy;
 
-    // Instantiate UART transmitter
-    uart_tx uart_inst (
+    uart uart_inst (
         .clk(clk),
         .send(send),
         .data(tx_byte),
@@ -31,7 +31,7 @@ module adc_to_uart (
     assign busy = uart_busy;
 
     always @(posedge clk) begin
-        send <= 0;  // default low unless sending
+        send <= 0;  // Default
 
         if (sample_delay < SAMPLE_DELAY) begin
             sample_delay <= sample_delay + 1;
@@ -40,18 +40,19 @@ module adc_to_uart (
 
             case (state)
                 0: begin
-                    tx_byte <= 8'hAA; // Start byte
+                    adc_raw <= i_xdac[15:4];                  // Mask: get true 12-bit value
+                    adc_sample <= {4'b0000, i_xdac[15:4]};    // Pad lower bits
+                    tx_byte <= 8'hAA;                         // Start byte
                     send <= 1;
-                    adc_sample <= i_xdac; // Capture XADC data
                     state <= 1;
                 end
                 1: begin
-                    tx_byte <= adc_sample[15:8]; // MSB
+                    tx_byte <= adc_sample[15:8];              // MSB
                     send <= 1;
                     state <= 2;
                 end
                 2: begin
-                    tx_byte <= adc_sample[7:0];  // LSB
+                    tx_byte <= adc_sample[7:0];               // LSB
                     send <= 1;
                     state <= 0;
                 end
--- mic_to_led.v
+++ mic_to_led.v
@@ -1,12 +1,12 @@
 `timescale 1ns/1ps
 
-module (
+module mic_to_led (
     input i_dig_mic,
     output o_led0,
     output o_led1
 );
 
 assign o_led0 = !i_dig_mic;
-assign o_led1 = i_dig_mic
+assign o_led1 = i_dig_mic;
 
 endmodule
